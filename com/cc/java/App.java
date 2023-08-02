1881df9867a978cd33fba33c7f722d2c7868dcdf
Yasmeen <yasmeenmas@gmx.de>
2023-08-02 11:11:57 +0200

    3. Iteration :: Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -26,27 +26,15 @@ public class App {
         
     }
 
-    // --- 2. Iteration : : Overloading ---//
-    private static void pollObj(Queen obj) {
+    // --- 3. Iteration :: Polymorphie ---//
+    private static void pollObj(HoneyBee obj) {
         output(obj.doYourJob());
         output(obj.fly());
         output("--------------------"); 
 
     }
 
-     private static void pollObj(Worker obj) {
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("--------------------"); 
 
-    }
-
-    private static void pollObj(Drone obj) {
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("--------------------"); 
-
-    }
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
